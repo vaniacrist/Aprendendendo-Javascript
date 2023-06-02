f99dc7cf99d367578b64bab8027c6296cfb06982
vaniacristinadossantos <vania.ggdossantos@gmail.com>
2023-06-02 11:47:05 -0300

    aprendendendo urna-eletronica.js

---
+++ estruturas.js
@@ -0,0 +1,22 @@
+function escrevenoconsole() {
+    console.log("mensagem no console");
+
+}
+
+function escreveNoAlert() {
+    window.alert("Mensagem noalert\n outra Mensagem\n\nmais uma mensagem");
+    
+
+}
+
+function escreveNoDocumento() {
+    document.write("Mensagem no documento");
+}
+
+function urnaEletronica() {
+
+}  
+function barulhinho() {
+    let barulhinho = new Audio("confirma-urna.mp3");
+    barulhinho.play();
+}
---
+++ urnaeltronica.js
@@ -0,0 +1,19 @@
+function inicio() {
+    let numeroDeVoto
+    let numeroDeVotoCandidato1=0
+    let numeroDeVotoCandidato2=0
+    let numeroDeVotoCandidato3=0
+    let numeroDeVotonulo=0
+    let somaTotaldeVotos=0
+
+}
+        do {
+
+            console.log("1 <-candidato1 \n")
+            console.log("1 <-candidato2 \n")
+            console.log("1 <-candidato3 \n")
+            console.log("5 <- voto em branco\n")
+            console.log("8 <- voto nulo\n")
+        }
+ 
+
